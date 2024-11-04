6b35cb2e5430da5f39169fc2a691acc2ed09c3bf
Marina Shumilina <mari.an.shum@yandex.ru>
2024-11-04 13:57:14 +0300

    WIP

--- alaska/main.py
+++ alaska/main.py
@@ -2,7 +2,6 @@ from alaska.preparers.preparers import preparers
 from alaska.processing.discourse_analysis.discourse_analysis import discourse_analysis
 from alaska.processing.alaskan_russian.alaskan_russian import alaskan_russian
 
-
 preparers()
 discourse_analysis()
 alaskan_russian()
